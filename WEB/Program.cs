5087379e70b7f47a82ef827da2bb9c5e03e9b5f6
hsynkyn <lossesrose0321@gmail.com>
2025-08-05 23:49:12 +0300

    05.08.2025 düzeltmeler yapıldı

--- WEB/Program.cs
+++ WEB/Program.cs
@@ -81,6 +81,6 @@ app.MapControllerRoute(
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
